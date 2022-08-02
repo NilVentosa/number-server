dc2642181164457eb63a748b4dbf419b2bb41d5c
Nil Ventosa <nil.ventosa@sword-group.com>
2022-08-02 14:04:56 +0200

    Add Constants class

--- src/main/java/xyz/ventosa/NumberServerCommand.java
+++ src/main/java/xyz/ventosa/NumberServerCommand.java
@@ -2,20 +2,32 @@ package xyz.ventosa;
 
 import picocli.CommandLine;
 import picocli.CommandLine.*;
+import xyz.ventosa.server.NumberServer;
 
 import java.util.concurrent.Callable;
+
+import static xyz.ventosa.util.Constants.*;
 
 @Command(name = "number-server", mixinStandardHelpOptions = true,
     version = "number-server 1.0", description = "Starts a number server.", showDefaultValues = true)
 public class NumberServerCommand implements Callable<Integer> {
 
-    @Option(names = {"-p", "--port"}, description = "The port the server will listen to.", defaultValue = "4000")
+    @Option(
+            names = {"-p", "--port"},
+            description = "The port the server will listen to.",
+            defaultValue = DEFAULT_PORT)
     private int port;
 
-    @Option(names = {"-m", "--max-connections"}, description = "The maximum number of concurrent clients.", defaultValue = "5")
+    @Option(
+            names = {"-m", "--max-connections"},
+            description = "The maximum number of concurrent clients.",
+            defaultValue = DEFAULT_MAX_CONCURRENT_CLIENTS)
     private int maxConcurrentConnections;
 
-    @Option(names = {"-r", "--report-frequency"}, description = "How often (in milliseconds) the report will be printed.", defaultValue = "10000")
+    @Option(
+            names = {"-r", "--report-frequency"},
+            description = "How often (in milliseconds) the report will be printed.",
+            defaultValue = DEFAULT_REPORT_FREQUENCY)
     private int reportFrequency;
 
     public static void main(String[] args) {
---
+++ src/main/java/xyz/ventosa/util/Constants.java
@@ -0,0 +1,8 @@
+package xyz.ventosa.util;
+
+public class Constants {
+
+    public static final String DEFAULT_PORT = "4000";
+    public static final String DEFAULT_MAX_CONCURRENT_CLIENTS = "5";
+    public static final String DEFAULT_REPORT_FREQUENCY = "10000";
+}
